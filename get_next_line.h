903f29c576a80aca4e30438ea24c55fa90ff5770
mozahnou <mozahnou@student.1337.ma>
2024-12-09 13:51:31 +0100

    chimex

--- get_next_line.h
+++ get_next_line.h
@@ -1,12 +1,13 @@
 #ifndef GET_NEXT_LINE_H
 #define GET_NEXT_LINE_H
 
+#ifndef BUFFER_SIZE
 #define BUFFER_SIZE 5
 
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
-
+#include <stdio.h>
 char *get_next_line(int fd);
 char	*read_str(int fd, char *str);
 int ft_strlen(char *str);
@@ -14,6 +15,9 @@ char	*ft_strjoin(char *s1, char *s2);
 int new_line(char *str);
 char	*ft_first_line(char *first_str);
 char	*ft_last_line(char *last_str);
+
+#endif
+
 
 
 #endif
