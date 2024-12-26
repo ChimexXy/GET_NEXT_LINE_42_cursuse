557da8bd2e753aebdc3da1ebb13bbe21d42ee3a5
chimexXy <moadzahnoun37@gmail.com>
2024-12-26 12:27:18 +0100

    chimex

---
+++ get_next_line.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mozahnou <mozahnou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/13 20:36:22 by mozahnou          #+#    #+#             */
+/*   Updated: 2024/12/13 20:41:38 by mozahnou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+int		ft_strlen(char *str);
+char	*ft_strdup(char *s);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_substr(char *string, int start, int len);
+int		ft_find_new_line(char *str);
+char	*ft_read_string(int fd, char *ret);
+char	*ft_ret_line(char *line);
+char	*ft_remind_val(char *string);
+
+#endif
